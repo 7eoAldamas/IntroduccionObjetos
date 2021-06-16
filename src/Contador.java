a126a7388acea57757a3db278f1e720a01f50ae0
Leo Aldamas <leo.aldamas@gmail.com>
2021-06-16 10:44:03 -0600

    "Clase Contador - Completada"

--- src/Contador.java
+++ src/Contador.java
@@ -1,42 +1,42 @@
 public class Contador {
 //---
 
-    private int incremento;
-    private int decremento;
+    private int contador;
+    private int auxcontador;
 
 //--- Constructo de Contador
-    public Contador (){ //Constructor por defecto
+    public Contador (String nombre){ //Constructor por defecto
+        System.out.println(nombre+"\n");
     }
 
-    public Contador (int incremento, int decremento){ //Constructor con parámetros
-        this.incremento = incremento;
-        this.decremento = decremento;
+    public Contador (String nombre, int contador){ //Constructor con parámetros
+        this(nombre);
+        this.contador = contador;
+        this.auxcontador = contador;
     }
 
     //--- Métodos de Acceso
-    public int getIncremento() {
-        return incremento;
+    public int getContador() {
+        return contador;
     }
 
-    public void setIncremento(int incremento) {
-        this.incremento = incremento;
-    }
-
-    public int getDecremento() {
-        return decremento;
-    }
-
-    public void setDecremento(int decremento) {
-        this.decremento = decremento;
+    public void setContador(int contador) {
+        this.contador = contador;
     }
 
     //--- Método para incrementar un número -> num = num + 1
-    public void incrementarNumero(int incremento){
-        System.out.println(incremento++);
+    public int incrementarNumero(){
+        return ++contador;
     }
 
     //--- Método para decrementar un número -> num = num - 1
-    public int decrementarNumero(int decremento){
-        return this.decremento = decremento--;
+    public int decrementarNumero(){
+        return --auxcontador;
+    }
+
+    //--- Método para mostrar el incremento y decremento
+    public void resultadoInDe(){
+        System.out.println("*-* El incremento del numero es: "+incrementarNumero());
+        System.out.println("*-* El decremento del numero es: "+decrementarNumero());
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
     //---
     public static void main(String[] args) {
         //--- Código Ejecutable
-
+        Contador c1 = new Contador("Clase Contador", 6);
+        c1.resultadoInDe();
     }
 }
