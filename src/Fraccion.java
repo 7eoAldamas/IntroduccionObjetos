269698f09461c9e8d5807f3b9f2dd3a8a8ad1f4b
Leo Aldamas <leo.aldamas@gmail.com>
2021-06-16 12:17:10 -0600

    "Clase Fraccion - Completada"

--- src/Fraccion.java
+++ src/Fraccion.java
@@ -1,2 +1,72 @@
 public class Fraccion {
+//---
+
+    private int numerador;
+    private int denominador;
+
+//--- Constructor Fraccion
+    public Fraccion (){ //Constructor por defecto
+    }
+
+    public Fraccion (int numerador, int denominador){ //Constructor con parámetros
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    //--- Métodos de Acceso
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    //--- Método para sumar fracción
+    public void sumaFraccion(Fraccion frac){
+        Fraccion aux = new Fraccion();
+        aux.numerador = (this.numerador * frac.denominador) + (this.denominador * frac.numerador);
+        aux.denominador = (this.denominador * frac.denominador);
+
+        System.out.println("*-*\tLa suma es:  "+aux.numerador+"/"+aux.denominador);
+    }
+
+    //--- Método para restar fracción
+    public void restaFraccion(Fraccion frac){
+        Fraccion aux = new Fraccion();
+        aux.numerador = (this.numerador * frac.denominador) - (this.denominador * frac.numerador);
+        aux.denominador = (this.denominador * frac.denominador);
+
+        System.out.println("*-*\tLa resta es:  "+aux.numerador+"/"+aux.denominador);
+    }
+
+    //--- Método para multiplicación de fracción
+    public void multiFraccion(Fraccion frac){
+        Fraccion aux = new Fraccion();
+        aux.numerador = (this.numerador * frac.numerador);
+        aux.denominador = (this.denominador * frac.denominador);
+
+        System.out.println("*-*\tLa multiplicacion es:  "+aux.numerador+"/"+aux.denominador);
+    }
+
+    //--- Método para la división fracción
+    public void divFraccion(Fraccion frac){
+        Fraccion aux = new Fraccion();
+        if (frac.denominador!=0) {
+            aux.numerador = (this.numerador * frac.denominador);
+            aux.denominador = (this.denominador * frac.numerador);
+
+            System.out.println("*-*\tLa division es:  " + aux.numerador + "/" + aux.denominador);
+        } else {
+            System.out.println("*-* Error *-*");
+        }
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,13 @@ public class Principal {
     //---
     public static void main(String[] args) {
         //--- Código Ejecutable
+        Fraccion f1 = new Fraccion(1, 6);
+        Fraccion f2 = new Fraccion(5, 2);
+        f1.sumaFraccion(f2);
+        f1.restaFraccion(f2);
+        f1.multiFraccion(f2);
+        f1.divFraccion(f2);
+
 
     }
 }
